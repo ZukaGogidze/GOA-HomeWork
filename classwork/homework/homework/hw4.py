0f55fa9d17ce4a7c9a73322476585570d885b506
ZukaGogidze <zw2p8pywqn@privaterelay.appleid.com>
2025-11-04 05:18:55 -0800

    first commit

---
+++ classwork/homework/homework/hw4.py
@@ -0,0 +1,19 @@
+#4) მომხმარებელს შემოატანინეთ 3 რიცხვი, ჯერ დაბეჭდეთ მათი ჯამი, შემდეგ კი გადაიყვანეთ თითოეული მათგანი string მონაცემთა ტიპად, მოახდინეთ მათ შორის კონკადინაცია და დაბეჭდეთ
+
+num1 = int(input("welcome"))
+
+num2 = int(input("Hello"))
+
+num3 = int(input("World"))
+
+sum = num1 + num2 + num3 
+print("sum is ",sum)
+
+num1_str = str(num1)
+
+num2_str = str(num2)
+
+num3_str = str(num3)
+
+StrSum = num1_str + num2_str + num3_str
+print("Concad",StrSum)
---
+++ classwork/lvl10/hw5.py
@@ -0,0 +1,5 @@
+#6) მომხმარებელს შემოატანინეთ სახელი, შეინახეთ იგი ცვლადში და მკაცრად შეამოწმეთ უდრის თუ არა იგი თქვენს სახელს.
+
+username = str(input("Enter The Name: "))
+Myname = "Zuka" 
+print(username == Myname)
