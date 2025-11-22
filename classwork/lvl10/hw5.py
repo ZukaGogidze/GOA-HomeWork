d02830b2ea95fd0448f5f7611b26e960877aae6d
ZukaGogidze <zw2p8pywqn@privaterelay.appleid.com>
2025-11-22 03:57:44 -0800

    second commit

--- classwork/lvl10/hw5.py
+++
@@ -1,5 +0,0 @@
-#6) მომხმარებელს შემოატანინეთ სახელი, შეინახეთ იგი ცვლადში და მკაცრად შეამოწმეთ უდრის თუ არა იგი თქვენს სახელს.
-
-username = str(input("Enter The Name: "))
-Myname = "Zuka" 
-print(username == Myname)
