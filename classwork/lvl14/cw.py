e563545edb5ccf3ff5b84b974912bdce7ab82ad8
ZukaGogidze <zw2p8pywqn@privaterelay.appleid.com>
2025-11-22 03:56:01 -0800

    second commit

---
+++ classwork/lvl14/cw.py
@@ -0,0 +1,15 @@
+# for i in range(100):
+#     print(i)
+
+# for i in range(101):
+#     print(i)
+
+
+# for i in range(5):
+#     num = int(input("Enter The Number: "))
+#     print(num)
+  
+
+
+for i in range(10,100):
+    print(i)
