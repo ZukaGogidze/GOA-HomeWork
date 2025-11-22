444f8b3cc3fd580415938785662fb1d838ee8873
ZukaGogidze <zw2p8pywqn@privaterelay.appleid.com>
2025-11-22 03:49:49 -0800

    second commit

---
+++ classwork/lvl 10/homework/hw1.py
@@ -0,0 +1,28 @@
+#2) კომენტარების სახით ჩამთვალეთ ყველა შედარებითი ოპერატორები და გააკეთეთ 5-5 ნაგალითი თითოეულზე.
+
+
+# <, >, <=, >=, <!=, >!=.
+
+# a = 17
+# b = 20
+# print(a == b)
+
+# age = 15
+# limit = 18
+# print(age != limit)
+
+# num1 = 17
+# num2 = 12
+# print(num1 > num2)
+
+# number = 23
+# number2 = 24
+# print(number < number)
+
+# age = 20
+# age2 = 30
+# print(age <= age2)
+
+# num1 = 25
+# num2 = 25
+# print(num1 >= num2)
